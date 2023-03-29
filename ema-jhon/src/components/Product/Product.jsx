df6fae5990d74f61e0e26c1ea38a7e681a6e9367
Akas_Datta <012akasdatta@gmail.com>
2023-03-29 20:35:58 +0600

    button click created

--- ema-jhon/src/components/Product/Product.jsx
+++ ema-jhon/src/components/Product/Product.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
+    // console.log(props);
     const {img, name, price, seller, ratings} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+  
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -11,7 +14,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Rating: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart</button>
         </div>
     );
 };
